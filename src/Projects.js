7204e30ed61f5c9b617299a903c1925707f0ecb2
joel7448 <joel.joel52@gmail.com>
2022-09-21 00:07:42 +0530

    changed

--- src/Projects.js
+++ src/Projects.js
@@ -87,15 +87,47 @@ function Projects(){
             <div className="col-lg-6 mt-5  ">
                 <h3 className="mt-lg-5">Online Nursery platform</h3>
 
-                <h5>Reactjs / Nodejs / MongoDB / JWT / Localstorage /Redux</h5>
+                <h5>Reactjs / Nodejs / MongoDB / JWT / Localstorage /Redux/ nodemailer</h5>
                <div className="links"> <a target="_blank" href="https://github.com/joel7448/ecommerce-frontend" className="GITHUB"><GitHub/></a>
                 <a target="_blank" href="https://ecommerce-frontend-tawny.vercel.app/" className="ui"><Widgets/></a></div>
                 </div>
                 </div>
             </div>
             
+            
            
         </div>
+        <div className="col-lg-6 pt-5 ps-2">
+            <div className="container text-center p-4"  data-aos-once="true" data-aos="fade-left">
+                <div className="row">
+                <div className="col-lg-6 ">
+                <Player
+          className="player1 ecommerce"
+          autoplay
+          loop
+          src="https://assets2.lottiefiles.com/packages/lf20_e1y24wm2.json"
+          style={{ height: "100px", width: "100px" }}
+        >
+          <Controls
+            visible={false}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
+        </Player>
+            </div>
+            <div className="col-lg-6 mt-5  ">
+                <h3 className="mt-lg-5">Online Nursery platform</h3>
+
+                <h5>Reactjs / Nodejs / MongoDB / JWT / Localstorage /Redux</h5>
+               <div className="links"> <a target="_blank" href="https://github.com/joel7448/nodemailerapp" className="GITHUB"><GitHub/></a>
+                <a target="_blank" href="https://nodemailerapp.netlify.app/" className="ui"><Widgets/></a></div>
+                </div>
+                </div>
+            </div>
+            
+            
+           
+        </div>
+        
  </div>
  
    
