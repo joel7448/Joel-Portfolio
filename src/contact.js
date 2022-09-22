6539e532fd14cf04098be64f2d54a37cc25ca8d9
joel7448 <joel.joel52@gmail.com>
2022-09-22 17:21:33 +0530

    changed

--- src/contact.js
+++ src/contact.js
@@ -3,8 +3,9 @@ function Contact(){
     
 
         <a target="_blank" href = "https://github.com/joel7448"><i class="fa-brands fa-github fa-2x m-4"></i></a>
-       <a target="_blank" href="https://twitter.com/JoelJerene"> <i class="fa-brands fa-twitter fa-2x m-4"></i></a>
+       <a target="_blank" href="https://stackoverflow.com/users/20061179/joel-jerene"> <i class="fa-brands fa-stack-overflow fa-2x m-4"></i></a>
         <a target="_blank" href="https://www.linkedin.com/in/joel-a-b3a991198/"><i class="fa-brands fa-linkedin fa-2x m-4"></i></a>
+        
     </div>)
 
 }
